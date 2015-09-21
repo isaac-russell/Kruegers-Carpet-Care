8f848e224bebb3e588fe4993361828152fb8a86d
Isaac Russell <irussell.ny@gmail.com>
2015-09-21 13:34:55 -0700

    Testing dev branch
    Added exclamation point.

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
 		<p>Why superior service statement goes here. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>		
 		</section>
 		
-		<a href="http://kruegarscarpetcare.com/pricing">Get an instant quote</a>';
+		<a href="http://kruegarscarpetcare.com/pricing">Get an instant quote!</a>';
 
 ?>
 
